f00cdd6048991b3108dc9518936a7b533c54208e
Matzen65 <matzeliasson@gmail.com>
2025-02-10 17:12:48 +0100

    main password

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 from user import User
 
-my_user = User("svenska", "Matz", "123")
+my_user = User( "svenska", "Matz", "123" )
 
 print( my_user )
 print( my_user.preferred_language )
@@ -10,3 +10,7 @@ my_user.preferred_language = "norska"
 print( my_user )
 
 print( my_user.get_password() )
+
+my_user.is_correct_login( "Kajsa", "060606" )
+
+print( my_user.password )
--- src/user.py
+++ src/user.py
@@ -9,8 +9,19 @@ class User:
     def __str__(self):
         """Returnerar klassens innehåll i strängformat"""
         return (f"user: {self.preferred_language}, {self.username}, "
-                f"{self.__password}")
+                f"{self.__password}") #__ betyder ej publict = dolt
 
+    # Ett alternativ är att skapa en "getter" med dekoratorn @property
     def get_password(self):
         return self.__password
 
+    @property
+    def password(self):
+        print("nu anropas 'password' getter")
+        return self.__password
+
+    def is_correct_login(self, username, password):
+        if username == self.username and password == self.__password:
+            return True
+        else:
+            return False
